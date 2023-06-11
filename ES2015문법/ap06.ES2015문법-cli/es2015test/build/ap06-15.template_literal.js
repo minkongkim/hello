7440ff9da1f911d4c69962fbbde50605e1d801c7
minkongkim <krkmk@naver.com>
2023-06-11 18:12:27 +0900

    23-06-11

--- ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 
   ES2015의 template literal( 백틱, ` ) 에 대해서 알아본다.
@@ -5,3 +7,5 @@
   1. 다중 문자열 만들 때
   2. 변수 치환
 */
+
+var str = "\na\n\nb\n134\n\n  ".concat(string1, ", ").concat(string2, "\n");
