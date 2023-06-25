eb776f35ebcbbc6c29e43b6c89efa74b49d54a76
minkongkim <krkmk@naver.com>
2023-06-25 18:15:41 +0900

    0625

---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/components/vue3701/CompChild2.vue
@@ -0,0 +1,25 @@
+<style scoped>
+.main {
+  border: 1px solid black;
+  background-color: aqua;
+}
+</style>
+
+<template>
+  <div class="main">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+// vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
+// import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      msg: 'child2',
+    };
+  },
+};
+</script>
--- Vuejs/vue30컴포넌트실습/comprj01/src/store/index.js
+++ Vuejs/vue30컴포넌트실습/comprj01/src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore } from 'vuex';
 
+import counterStore from './modules/counterStore.js';
+
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // actions: {},
+  // mutations: {},
+  // state: {},
+  // getters: {},
+  modules: {
+    // "모듈명: store명", 형태로 저장됩니다.
+    counterStore: counterStore,
+  },
 });
